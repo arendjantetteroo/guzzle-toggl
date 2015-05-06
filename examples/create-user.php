05d2621615a5b62b87086b0493052a35521769c7
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2015-05-06 20:34:20 +0200

    Add a create User and getUser example

---
+++ examples/create-user.php
@@ -0,0 +1,25 @@
+<?php
+
+require dirname(__FILE__). '/../apikey.php';
+
+require dirname(__FILE__).'/../vendor/autoload.php';
+
+use AJT\Toggl\TogglClient;
+
+// Get the toggl client with your toggl api key
+$toggl_client = TogglClient::factory(array('api_key' => $toggl_api_key, 'apiVersion' => $toggl_api_version, 'debug' => true));
+
+// Create a user
+print "createUser\n";
+$user = $toggl_client->createUser(['user' => [
+    'email' => 'user@example.com',
+    'password' => 'abcdefghi',
+]
+]);
+
+var_dump($user);
+
+// Get the current user
+$currentUser = $toggl_client->GetCurrentUser();
+
+var_dump($currentUser);
